b01b19df58fed17197da9b0dc4d2e35494511f44
Marvin Leiers <marvinleiers@gmail.com>
2019-11-17 21:53:13 +0100

    Initial commit

--- src/main/java/de/marvinleiers/binancebot/BinanceBot.java
+++ src/main/java/de/marvinleiers/binancebot/BinanceBot.java
@@ -15,29 +15,16 @@ class BinanceBot
         BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance();
         BinanceApiRestClient client = factory.newRestClient();
 
-
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Bitte gebe ein Symbol ein!\n>");
         String input = scanner.nextLine();
 
         List<Candlestick> candlesticks = client.getCandlestickBars(input.toUpperCase(), CandlestickInterval.HOURLY);
-        //System.out.println(candlesticks);
 
         for (Candlestick candlestick : candlesticks)
         {
             System.out.println(candlestick.getClose());
         }
-
-        /*while (true)
-        {
-            System.out.println(client.get24HrPriceStatistics(input).getLastPrice());
-            Thread.sleep(1000);
-        }*/
-    }
-
-    private float calculateMovingAverage()
-    {
-        return 0f;
     }
 }
