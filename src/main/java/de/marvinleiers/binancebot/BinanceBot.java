e8f57413cbfc74e489b26a05ed5ce54169b91fd5
Marvin Leiers <marvinleiers@gmail.com>
2019-11-17 21:36:11 +0100

    Initial commit

--- src/main/java/de/marvinleiers/binancebot/BinanceBot.java
+++ src/main/java/de/marvinleiers/binancebot/BinanceBot.java
@@ -2,26 +2,42 @@ package de.marvinleiers.binancebot;
 
 import com.binance.api.client.BinanceApiClientFactory;
 import com.binance.api.client.BinanceApiRestClient;
+import com.binance.api.client.domain.market.Candlestick;
+import com.binance.api.client.domain.market.CandlestickInterval;
 
+import java.util.List;
 import java.util.Scanner;
 
 class BinanceBot
 {
-    BinanceBot() throws InterruptedException
+    BinanceBot()
     {
         BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance();
         BinanceApiRestClient client = factory.newRestClient();
 
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Bitte gebe ein Symbol ein!\n>");
-
         String input = scanner.nextLine();
 
-        while (true)
+        List<Candlestick> candlesticks = client.getCandlestickBars(input.toUpperCase(), CandlestickInterval.HOURLY);
+        //System.out.println(candlesticks);
+
+        for (Candlestick candlestick : candlesticks)
+        {
+            System.out.println(candlestick.getClose());
+        }
+
+        /*while (true)
         {
             System.out.println(client.get24HrPriceStatistics(input).getLastPrice());
             Thread.sleep(1000);
-        }
+        }*/
+    }
+
+    private float calculateMovingAverage()
+    {
+        return 0f;
     }
 }
