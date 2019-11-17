4713ac1d915ddaa56a9aab7a01cfc579c6dc0788
Marvin Leiers <marvinleiers@gmail.com>
2019-11-17 23:21:27 +0100

    Trend detection function added

--- src/main/java/de/marvinleiers/binancebot/BinanceBot.java
+++ src/main/java/de/marvinleiers/binancebot/BinanceBot.java
@@ -10,21 +10,47 @@ import java.util.Scanner;
 
 class BinanceBot
 {
+    BinanceApiClientFactory factory;
+    BinanceApiRestClient client;
+    String input = "";
+
     BinanceBot()
     {
-        BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance();
-        BinanceApiRestClient client = factory.newRestClient();
+        factory = BinanceApiClientFactory.newInstance();
+        client = factory.newRestClient();
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Bitte gebe ein Symbol ein!\n>");
-        String input = scanner.nextLine();
+        input = scanner.nextLine();
 
-        List<Candlestick> candlesticks = client.getCandlestickBars(input.toUpperCase(), CandlestickInterval.HOURLY);
+        float currentPrice = Float.valueOf(client.get24HrPriceStatistics(input.toUpperCase()).getLastPrice());
 
-        for (Candlestick candlestick : candlesticks)
+        if (currentPrice > calculateSimpleMovingAverage(input.toUpperCase(), 10))
         {
-            System.out.println(candlestick.getClose());
+            System.out.println("Upwards trend detected! Current price: " + currentPrice + " MA: " + calculateSimpleMovingAverage(input.toUpperCase(), 10));
         }
+        else
+        {
+            System.out.println("No upwards trend detected!");
+        }
+    }
+
+    private float calculateSimpleMovingAverage(String asset, int durationInHours)
+    {
+        List<Candlestick> candlesticks = client.getCandlestickBars(asset.toUpperCase(), CandlestickInterval.HOURLY);
+        List<?> inDuration = candlesticks.subList(candlesticks.size() - durationInHours, candlesticks.size());
+
+        int amount = inDuration.size();
+        float toDivide = 0;
+
+        for (Object obj : inDuration)
+        {
+            Candlestick candlestick = (Candlestick) obj;
+
+            toDivide += Float.valueOf(candlestick.getClose());
+        }
+
+        return toDivide / amount;
     }
 }
